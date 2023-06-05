10cc3eb3f17245be780519870a1d2bceeae72e53
spansolusi <103178253+spansolusi@users.noreply.github.com>
2023-06-05 14:26:00 +0700

    Create usr2-sard-openrc.sh

---
+++ usr2-sard-openrc.sh
@@ -0,0 +1,16 @@
+# Ansible managed
+
+# Clear any old environment that may conflict.
+for key in $( set | awk '{FS="="}  /^OS_/ {print $1}' ); do unset $key ; done
+export OS_PROJECT_DOMAIN_NAME=Default
+export OS_USER_DOMAIN_NAME=Default
+export OS_PROJECT_NAME=project-demo_2
+export OS_TENANT_NAME=project-demo_2
+export OS_USERNAME=user-demo_2
+export OS_PASSWORD=1234
+export OS_AUTH_URL=http://10.6.82.254/identity/v3/
+export OS_INTERFACE=internal
+export OS_ENDPOINT_TYPE=internalURL
+export OS_IDENTITY_API_VERSION=3
+export OS_REGION_NAME=RegionOne
+export OS_AUTH_PLUGIN=password
