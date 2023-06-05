f96c7e12665597ee2943121e8ad62f124f0fc307
spansolusi <103178253+spansolusi@users.noreply.github.com>
2023-06-05 13:58:22 +0700

    Add files via upload

---
+++ admin-fishos-openrc.sh
@@ -0,0 +1,16 @@
+# Ansible managed
+
+# Clear any old environment that may conflict.
+for key in $( set | awk '{FS="="}  /^OS_/ {print $1}' ); do unset $key ; done
+export OS_PROJECT_DOMAIN_NAME=Default
+export OS_USER_DOMAIN_NAME=Default
+export OS_PROJECT_NAME=admin
+export OS_TENANT_NAME=admin
+export OS_USERNAME=admin
+export OS_PASSWORD=8vi6imCvEbG9JHe2
+export OS_AUTH_URL=http://10.6.82.254/identity/v3/
+export OS_INTERFACE=internal
+export OS_ENDPOINT_TYPE=internalURL
+export OS_IDENTITY_API_VERSION=3
+export OS_REGION_NAME=RegionOne
+export OS_AUTH_PLUGIN=password
